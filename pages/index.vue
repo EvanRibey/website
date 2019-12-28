138e808cd97a95fee69dd4845538911df547c00c
EvanRibey <contact@evanribey.com>
2019-12-28 16:15:54 -0500

    increase profile on mobile

--- pages/index.vue
+++ pages/index.vue
@@ -158,8 +158,8 @@ export default {
   &__img {
     width: 20vw;
     height: 20vw;
-    min-width: 150px;
-    min-height: 150px;
+    min-width: 20rem;
+    min-height: 20rem;
     margin-right: 35px;
     border: 1px solid #2e2e2e;
     box-shadow: 0 1px 8px #999;

18e4f7b90a49ecf5e989b53d8860370a6fc569a4
EvanRibey <contact@evanribey.com>
2019-12-28 14:18:05 -0500

    reset package deps + remove other files for init for nuxtjs

--- webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-  , common = require('./webpack.common')
-  ;
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-
-});
